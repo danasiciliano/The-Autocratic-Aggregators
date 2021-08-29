249fb6330297ed7fca5058979337befcd7f58684
Aayog <1aayogkoirala@gmail.com>
2021-08-29 02:50:04 -0400

    Added all html, updated the forms to make it work

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from helper import get_questions, get_info_from_csv, get_links_from_service
 
 app = Flask(__name__)
 
 @app.route("/")
 def form():
-    return render_template("form.html")
+    questions = get_questions("questions.txt")
+    return render_template("form.html", questions=questions)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    answers = ""
+    req = request.form.get("0", False)
+    print(request.form)
+    resources_dict = get_info_from_csv("resources.csv")
+    links = get_links_from_service(resources_dict, req)
+    return render_template("result.html", links=list(links))
+
 
 def main():
     app.run(debug=True)
 
 if __name__=="__main__":
     main()
+
+
+
+answers_to_links = {
+    [1 ,2, 3] : "SNAP"
+}
---
+++ helper.py
@@ -0,0 +1,39 @@
+import csv
+
+def get_questions(filename):
+    form_questions = dict()
+    with open(filename) as f:
+        isQuestion = True
+        question = ""
+        options = []
+        lines = f.readlines()
+        for line in lines:
+            if line == "\n":
+                form_questions[question] = options
+                options = []
+                isQuestion = True
+            elif isQuestion:
+                question = line.rstrip()
+                isQuestion = False
+            else:
+                options.append(line.rstrip())
+    return form_questions
+
+def get_info_from_csv(filename):
+    # create a dictionary
+    data = dict()
+     
+    # Open a csv reader called DictReader
+    with open(filename, encoding='utf-8') as f:
+        csvReader = csv.DictReader(f)
+        for rows in csvReader:
+            key = rows['ID']
+            data[key] = rows
+    return data
+
+def get_links_from_service(data, serviceType):
+    links = list()
+    for _, val in data.items():
+        if val['Service'].strip() == serviceType:
+            links.append({'Name': val['Name'], 'Link': val['Link'], 'Image': val['Image']})
+    return links
